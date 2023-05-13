6fc2f5673eed085e794d29cc1f830c5b94f7c5a0
h.young <cocotree221@gmail.com>
2023-05-13 17:21:22 +0900

    data fetching, from json
    api json , async await, future type

---
+++ lib/services/api_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
+      }
+      print(webtoonInstances);
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
